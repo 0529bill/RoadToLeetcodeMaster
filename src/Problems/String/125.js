9c81dec19aae100480e11ff4d252fcd353f2e550
bill <0529bilL@gmail.com>
2023-01-21 11:10:58 +0800

    feat: 125

---
+++ src/Problems/String/125.js
@@ -0,0 +1,52 @@
+//13. Roman to Integer
+
+/** function start here */
+
+var isPalindrome = function (s) {
+  let regex = /[a-z]/;
+  let result = s
+    .toLowerCase()
+    .split("")
+    .filter((val) => regex.test(val));
+  console.log("result", result);
+  return result.join("") === result.reverse().join("") ? true : false;
+};
+
+/** tests start here */
+
+test("test1", () => {
+  expect(isPalindrome("")).toBe(true);
+});
+
+test("test1", () => {
+  expect(isPalindrome("09")).toBe(true);
+});
+
+test("test2", () => {
+  expect(isPalindrome("aba")).toBe(true);
+});
+
+test("test3", () => {
+  expect(isPalindrome("a b ## a")).toBe(true);
+});
+
+test("test3", () => {
+  expect(isPalindrome("c")).toBe(true);
+});
+
+test("test3", () => {
+  expect(isPalindrome("ca")).toBe(false);
+});
+
+test("test3", () => {
+  expect(isPalindrome("ca cd ")).toBe(false);
+});
+
+//comments
+
+/**
+ *  1. input includes numbers and string
+ *
+ *
+ *
+ */
